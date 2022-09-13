3423e5edbed32c554bf4d6680ab4c6fb019eebe4
SeanRBurton <burton.seanr@gmail.com>
2022-09-13 12:10:38 +0100

    initial visualization

--- data/Information.py
+++ data/Information.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+import os
 
-data = pd.read_csv('/Users/giuliano/code/Slik300/forecasting-website/data/state_names_IND.csv')
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print("dir_path:", dir_path)
+
+data = pd.read_csv(os.path.join(dir_path, 'state_names_IND.csv'))
 STATES = data['NAME_1,C,22'].tolist()
