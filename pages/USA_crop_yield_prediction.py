e3374705231b42f817764cf54819f89b0d6017c4
SeanRBurton <burton.seanr@gmail.com>
2022-09-14 16:19:55 +0100

    add time-series scatter plot

--- pages/USA_crop_yield_prediction.py
+++ pages/USA_crop_yield_prediction.py
@@ -1,3 +1,3 @@
 from forecasting_website import interactive_map
 
-interactive_map.render('USA', 'USA', center = {'lat':  47.116386, 'lon': -101.299591})
+interactive_map.render('USA', 'USA', 'USA_corn', center = {'lat':  47.116386, 'lon': -101.299591})
