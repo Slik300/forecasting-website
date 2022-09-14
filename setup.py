0d602a80553da6f601fbf5af88d52153477075ca
SeanRBurton <burton.seanr@gmail.com>
2022-09-14 11:26:51 +0100

    add USA map

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
     content = f.readlines()
 requirements = [x.strip() for x in content]
 
-setup(name="forecasting-website",
+setup(name="forecasting_website",
       version="1.0",
       description="crop yield forecasting frontend",
       packages=find_packages(),
