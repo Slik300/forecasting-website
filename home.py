e8c83b6952eb3f63fcc386c3539f208edb0b3e34
SeanRBurton <burton.seanr@gmail.com>
2022-09-15 14:54:43 +0100

    update homepage

--- home.py
+++ home.py
@@ -1,17 +1,7 @@
 import streamlit as st
-import base64
 
 st.set_page_config(layout="wide")
 
 '''
-# Food Security Forecaster
+# Forecasting crop yields using deep learning on satellite imagery
 '''
-# Import libraries
-
-import ee
-import geemap.foliumap as geemap
-
-'''
-# India NDVI 2010-2013
-'''
-st.markdown("![Alt Text](https://earthengine.googleapis.com/v1alpha/projects/ee-slik300/videoThumbnails/21d1c5393e54d3d493900af609a59b3b-1c640589449c33cc9398e50f77d4ffc4:getPixels)")
